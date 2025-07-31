be0f79571ab5d9d44497f164524bcd7b4bf8c592
Ilhan Ozkan <contact.ilhanozkan@gmail.com>
2025-07-31 20:07:54 +0300

    Replace the ORM config with a JSON file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductTypeModule } from './product-type/product-type.module';
 import { UserModule } from './user/user.module';
 
+import ormConfig from '../ormconfig.json';
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      database: 'packaging_supply_system',
-      username: 'postgres',
-      password: 'postgres',
-      synchronize: true,
-      logging: true,
-      entities: ['./src/**/*.entity.ts', './src/**/*.entity.js'],
-    }),
+    TypeOrmModule.forRoot(ormConfig as TypeOrmModuleOptions),
     ProductTypeModule,
     UserModule,
   ],
