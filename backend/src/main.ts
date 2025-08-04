e054d1c110dac540d9cfb8e38433723f61a30b3e
Ilhan Ozkan <contact.ilhanozkan@gmail.com>
2025-08-04 16:16:01 +0300

    Enable cors in backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
 
   // ProductType mock data initialization
   const productTypeService = app.get(ProductTypeService);
